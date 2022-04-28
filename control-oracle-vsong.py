a3192c01148ba0172d40a6c3b43355a9cec13c90
bonjinnorenka <32708102+bonjinnorenka@users.noreply.github.com>
2022-04-29 00:49:40 +0900

    ちょっと機能追加

--- control-oracle-vsong.py
+++ control-oracle-vsong.py
@@ -18,6 +18,9 @@ while True:
         ov.true_check()
     elif nowcommand=="make_page":
         ov.make_all_musicpage()
+        ov.make_all_chpage()
+    elif nowcommand=="re_pic":
+        ov.reloadpeople_picture()
     elif nowcommand=="always_do":
         ov.correct_video_list()
         input("準備が完了したらキーを押してください")
@@ -32,4 +35,4 @@ while True:
     else:
         print("exit:終了\nhelp:今のコマンド\nupdate_vdata:動画の統計情報を更新します\nupdate_vlist:プレイリストから最新の動画idを取得します\nadd_chdata:足りないチャンネルを取得します\
         \nadd_gname:足りないグループ名を追加します\nadd_mdata:足りない音楽データを追加します\ntrue_check:データが正常に登録されているか確認します\nmake_page:ページを生成します\
-        \nalways_do:定期的に実行するコマンドをまとめたもの")
+        \nre_pic:チャンネルの顔画像を更新します\nalways_do:定期的に実行するコマンドをまとめたもの")
