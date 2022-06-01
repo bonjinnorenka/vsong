5555044e7fdb0e8c91f025e8338e1585284f0281
bonjinnorenka <32708102+bonjinnorenka@users.noreply.github.com>
2022-06-01 21:31:14 +0900

    cronで操作をできるように改良

--- control-oracle-vsong.py
+++ control-oracle-vsong.py
@@ -1,6 +1,11 @@
 import oracle_vsong as ov
+import sys
+
 while True:
-    nowcommand = input("vsong_maneger: ")
+    try:
+        nowcommand = sys.argv[2]
+    except:
+        nowcommand = input("vsong_maneger: ")
     if nowcommand=="exit":
         ov.conect_close()
         break
@@ -44,6 +49,23 @@ while True:
         ov.yt_status_ex()
         print("データの整合性をチェックしています")
         ov.true_check()
+    elif nowcommand=="always_do_server":
+        ov.update_videodata()
+        ov.add_ch_data()
+        ov.add_groupe_name()
+        ov.add_music_data()
+        ov.music_modify_update()
+        ov.ch_modify_update()
+        ov.yt_status_ex()
+        ov.true_check()
+        ov.reloadpeople_picture()
+        ov.make_search_index()
+        ov.make_all_musicpage()
+        ov.make_all_chpage()
+        ov.makesitemap()
+        ov.music_recommend_page()
+        ov.channel_recommend_page()
+        ov.make_music_top()
     elif nowcommand=="cplib":
         inpst = input("本当にライブラリデータを本番環境に反映してよろしいですか?(y/n)")
         if inpst=="y":
@@ -71,3 +93,11 @@ while True:
         \nadd_gname:足りないグループ名を追加します\nadd_mdata:足りない音楽データを追加します\ntrue_check:データが正常に登録されているか確認します\nmake_page:ページを生成します\
         \nre_pic:チャンネルの顔画像を更新します\nse_index:検索用索引を生成\nrandom_gen:おすすめデータ生成\nhot:人気動画ランキング生成\nmake_sitemap:サイトマップ生成\nreload_status:視聴できるかの状態を確認\
         \nalways_do:定期的に実行するコマンドをまとめたもの")
+    print("\ntask finish!")
+    try:
+        if nowcommand==sys.argv[2]:
+            ov.pro_log("log","cron-job-"+nowcommand,"","success finish!")
+            ov.conect_close()
+            break
+    except:
+        pass
