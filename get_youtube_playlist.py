2abe64c715404286dc06fe2955bf40dad48cf2bb
bonjinnorenka <32708102+bonjinnorenka@users.noreply.github.com>
2022-06-11 19:35:49 +0900

    mysqlに接続している時間を削減

--- get_youtube_playlist.py
+++ get_youtube_playlist.py
@@ -2,11 +2,11 @@ import MySQLdb
 import sys
 from pytube import Playlist
 import ev
-connection = MySQLdb.connect(host=ev.mysql_host,user=ev.mysql_user,passwd=ev.mysql_ps,db="vsong")
-cur = connection.cursor()
 playlist_id = str(sys.argv[1])
 playlist = Playlist("https://www.youtube.com/playlist?list=" + playlist_id)
 ins_playlist = [str(playlist[x])[32:] for x in range(len(playlist))]
+connection = MySQLdb.connect(host=ev.mysql_host,user=ev.mysql_user,passwd=ev.mysql_ps,db="vsong")
+cur = connection.cursor()
 cur.executemany("INSERT INTO vsong.tmp_vid (videoid) VALUES (%s)",ins_playlist)
 connection.commit()
 cur.close()
